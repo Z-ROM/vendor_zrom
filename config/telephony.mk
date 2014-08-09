LiquidSmooth-Old/android_vendor_liquid

World Phone Support
Credit: CM
You will need to remove common_gsm/common_cdma from your liquid.mk

config/telephony.mk
@@ -1,0 +1,21 @@
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/liquid/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/liquid/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+# Selective SPN list for operator number who has the problem. 
+PRODUCT_COPY_FILES += \
+    vendor/liquid/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
+# Telephony packages
+PRODUCT_PACKAGES += \
+    Mms \
+    Stk \
+    CellBroadcastReceiver \
+	
+# Mms depends on SoundRecorder for recorded audio messages
+PRODUCT_PACKAGES += \
+    SoundRecorder

Z-ROM/vendor_zrom

config/telephony.mk
@@ -1,0 +1,21 @@
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/zrom/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/zrom/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+# Selective SPN list for operator number who has the problem. 
+PRODUCT_COPY_FILES += \
+    vendor/zrom/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
+# Telephony packages
+PRODUCT_PACKAGES += \
+    Mms \
+    Stk \
+    CellBroadcastReceiver \
+	
+# Mms depends on SoundRecorder for recorded audio messages
+PRODUCT_PACKAGES += \
+    SoundRecorder
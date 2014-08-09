SlimRoms/vendor_slim

selective based spn/operatorCheck (2/2)
credit to invisiblek

Change-Id: Iffc338c0cd5db472799f7a81bd9222562e5dfef5

config/common_full_phone.mk
@@ -9,6 +9,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
 
+# Selective SPN list for operator number who has the problem.
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
 # SIM Toolkit
 PRODUCT_PACKAGES += \
     Stk

Z-ROM/vendor_zrom

config/common_full_phone.mk
@@ -13,6 +13,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/zrom/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
 
+# Selective SPN list for operator number who has the problem.
+PRODUCT_COPY_FILES += \
+    vendor/zrom/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
 # SIM Toolkit
 PRODUCT_PACKAGES += \
     Stk